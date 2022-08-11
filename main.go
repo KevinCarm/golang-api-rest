a48a277b259267e89515e69dbd74b17f4156259b
KevinCarm <kevincarm016@hotmail.com>
2022-08-11 12:36:21 -0500

    Add crud 'get all' funcionality

--- main.go
+++ main.go
@@ -3,34 +3,29 @@ package main
 import (
 	"log"
 
+	"gorm/connection"
+	"gorm/model"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
+var StringConnection string = "root:12345678@tcp(127.0.0.1:3306)/gorm_db"
+var DbConnection = connection.GetConnection(StringConnection)
 
-	/*stringConnection := "root:12345678@tcp(127.0.0.1:3306)/gorm_db"
-	dbConnection := connection.GetConnection(stringConnection)*/
+func main() {
 
 	app := fiber.New()
 
-	/*user := model.User{
-		Name:     "Miguel",
-		Email:    "miguel@correo.com",
-		Password: "12345678",
-	}
-
-	dbConnection.Create(&user)
-
-	users := []model.User{}
-
-	dbConnection.Find(&users)
-
-	fmt.Println(users)*/
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello")
-	})
+	app.Get("/", GetAll)
 
 	log.Fatal(app.Listen(":8080"))
 
+}
+
+func GetAll(c *fiber.Ctx) error {
+	users := []model.User{}
+
+	DbConnection.Find(&users)
+
+	return c.JSON(users)
 }
